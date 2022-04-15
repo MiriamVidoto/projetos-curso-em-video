72b8d3c9eb0fea72d7c94a35c866044d46d24bc3
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-04-15 20:07:22 -0300

    finalizado

--- projeto1/script.js
+++ projeto1/script.js
@@ -1,7 +1,20 @@
 function carregar(){
     var msg = document.getElementById('msg');
-    var foto = document.getElementById('foto');
+    var img = document.getElementById('image');
     var data = new Date();
     var hora = data.getHours();
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'images/manha.png'
+        document.body.style.background = '#A5D4DE';
+        document.getElementById('msg2').innerText = "Bom dia!";
+    }else if (hora >= 12 && hora<18){
+        img.src = 'images/tarde.png';
+        document.body.style.background = '#FCBE2E';
+        document.getElementById('msg2').innerText = "Boa tarde!";
+    }else{
+        img.src = 'images/noite.png'
+        document.body.style.background = '#223272';
+        document.getElementById('msg2').innerText = "Boa noite!";
+    }
 }
