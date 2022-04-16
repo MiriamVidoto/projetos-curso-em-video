afb26d374f9eb7954790bcf7f0d8c6b9fa029fa2
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-04-16 20:23:56 -0300

    configuracao html

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Olá');
+function gerarTabuada(){
+    let numeroEscolhido = document.getElementById('numero');
+    let tab = document.getElementById('seltab')
+    if (numeroEscolhido.value.length == 0) {
+        alert('Por favor digite um número!')        
+    } else{
+        let num = Number(numeroEscolhido)
+        for(let c = 1; c <= 10; c += 1){
+        let res = (num * c);
+        return res
+    }
+    }
+    
+}
